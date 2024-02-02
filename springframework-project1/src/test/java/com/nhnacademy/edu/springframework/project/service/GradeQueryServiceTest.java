bf9f785676bb99be4201436872bee0a74d450a8f
chanhwiim <chanhwiim459@gmail.com>
2024-02-02 13:48:36 +0900

    test code 수

--- springframework-project1/src/test/java/com/nhnacademy/edu/springframework/project/service/GradeQueryServiceTest.java
+++ springframework-project1/src/test/java/com/nhnacademy/edu/springframework/project/service/GradeQueryServiceTest.java
@@ -9,10 +9,13 @@ import com.nhnacademy.edu.springframework.project.service.DefaultGradeQueryServi
 import com.nhnacademy.edu.springframework.project.service.Student;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -22,17 +25,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest(classes = ServiceConfig.class)
+@ExtendWith(SpringExtension.class)
 class GradeQueryServiceTest {
 
-    @MockBean
+    @Mock
     private Students mockStudents;
 
-    @MockBean
+    @Mock
     private Scores mockScores;
 
-    @Autowired
-    private GradeQueryService gradeQueryService;
+    @InjectMocks
+    private DefaultGradeQueryService gradeQueryService;
+
+    @Test
+    void name() {
+        gradeQueryService.hashCode();
+    }
 
     @Test
     void getScoreByStudentName() {
